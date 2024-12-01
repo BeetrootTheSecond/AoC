e29a77f5f0332fb80f5700a25fafaf75d9c98e2d
beetroot <aaronhorrell@hotmail.co.uk>
2024-12-01 17:40:31 +0000

    add commander cli and updated day 1 to work with the commander

---
+++ index.ts
@@ -0,0 +1,34 @@
+import { Argument, Command } from 'commander';
+import fs from 'fs';
+
+
+import * as y2024 from './source/2024';
+
+const program = new Command();
+
+program.command('test')
+    .description('')
+    .addArgument(new Argument('<year>', 'Year').choices(['2024']))
+    .addArgument(new Argument('<day>', 'Day').choices(([...new Array(25)].map((_, i) => (i + 1).toString()))))
+    .addArgument(new Argument('<part>', 'Part').choices(['1','2','all']))
+    .option('-s, --sample', 'run using sample data',false)
+    .action((year,day:string,part,option)=>{
+        
+        console.log(`Year ${year} , Day ${day} , Part ${part} , Sample ${option.sample}`);
+        // load data
+
+        let data = fs.readFileSync(`.input/${year}/${day}.txt`, 'utf-8').split('\n');
+        if(option.sample){
+            data = fs.readFileSync(`.input/${year}/${day}.sample.txt`, 'utf-8').split('\n');
+        }
+        
+        
+
+        for(const [key, fn] of Object.entries(y2024)){
+            if(key.replace("day","") == day){
+                fn(data, part);
+                break;
+             }
+        }
+    })
+    .parse();
--- source/2024/day1.ts
+++ source/2024/day1.ts
@@ -1,24 +1,27 @@
-import fs from 'fs';
 
-const runSample = false;
+export const day1 = (data: string[], part: string )=> {
+
+
+
 //import the data into 2 arrays 
-console.log(__dirname)
-const sampleData = fs.readFileSync('.input/2024/1.sample.txt', 'utf-8');
-const data = fs.readFileSync('.input/2024/1.txt', 'utf-8');
-let runningData = data;
-if(runSample){
-    runningData = sampleData;
-}
-let lines = runningData.split('\n')
-let splitLine =lines.map(line => line.split('   '))
+
+// const sampleData = fs.readFileSync('.input/2024/1.sample.txt', 'utf-8');
+// const data = fs.readFileSync('.input/2024/1.txt', 'utf-8');
+
+let splitLine = data.map(line => line.split('   '))
 
 let leftList = splitLine.map(row =>parseInt(row[0])).sort();
 let rightList = splitLine.map(row =>parseInt(row[1])).sort();
 
+if(part == '1'){
 let listDiffences: number[] = leftList.map((location,index) => Math.abs(location - rightList[index]));
 let totalDistance = listDiffences.reduce((total,distance) =>  total + distance,0); 
 console.log(`Star One : ${totalDistance} `);
+return;
+}
 
 let similarityScores:number[]= leftList.map(location =>location * rightList.filter(value => value== location).length);
 let totalSimilarityScore = similarityScores.reduce((total,score) => total + score,0); 
 console.log(`Star Two : ${totalSimilarityScore} `);
+return;
+}
