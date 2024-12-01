805d15b47844b9ac23430cc0f908c219f215e664
beetroot <aaronhorrell@hotmail.co.uk>
2024-12-01 10:02:39 +0000

    Day 1 2016

--- 2016/day1.js
+++ 2016/day1.js
@@ -16,16 +16,16 @@ function starOneSolution() {
     };
     //count number of blocks away from starting position 
     day1Data_json_1.default.forEach(currentMove => {
-        let rotation = currentMove.slice(0, 1);
-        let numberOfBlocks = currentMove.slice(1);
+        let Rotation = currentMove.slice(0, 1);
+        let BlockCount = currentMove.slice(1);
         //handle rotation
-        if (rotation == 'R') {
+        if (Rotation == 'R') {
             facingdirection++;
             if (facingdirection == 4) {
                 facingdirection = 0;
             }
         }
-        else if (rotation == 'L') {
+        else if (Rotation == 'L') {
             facingdirection--;
             if (facingdirection == -1) {
                 facingdirection = 3;
@@ -36,19 +36,19 @@ function starOneSolution() {
         }
         switch (facingdirection) {
             case 0: { //north
-                blockDirectionCount.north += parseInt(numberOfBlocks);
+                blockDirectionCount.north += parseInt(BlockCount);
                 break;
             }
             case 1: { //east
-                blockDirectionCount.east += parseInt(numberOfBlocks);
+                blockDirectionCount.east += parseInt(BlockCount);
                 break;
             }
             case 2: { //south
-                blockDirectionCount.north -= parseInt(numberOfBlocks);
+                blockDirectionCount.north -= parseInt(BlockCount);
                 break;
             }
             case 3: { //west
-                blockDirectionCount.east -= parseInt(numberOfBlocks);
+                blockDirectionCount.east -= parseInt(BlockCount);
                 break;
             }
         }
@@ -57,4 +57,69 @@ function starOneSolution() {
     let totalBlocksAway = Math.abs(blockDirectionCount.north) + Math.abs(blockDirectionCount.east);
     console.log(`Star One : ${totalBlocksAway}`);
 }
+function starTwoSolution() {
+    let testData3 = ['R5', 'L5', 'R5', 'R3'];
+    let facingdirection = 0; //0-N, 1-E, 2-S, 3-W
+    let blockDirectionCount = {
+        north: 0,
+        east: 0
+    };
+    let blockDirectionCountFirstPosition = {
+        north: 0,
+        east: 0
+    };
+    let foundFlag = false;
+    let visitedPosition = [];
+    //count number of blocks away from starting position 
+    day1Data_json_1.default.forEach(currentMove => {
+        let Rotation = currentMove.slice(0, 1);
+        let BlockCount = currentMove.slice(1);
+        //handle rotation
+        if (Rotation == 'R') {
+            facingdirection++;
+            if (facingdirection == 4) {
+                facingdirection = 0;
+            }
+        }
+        else if (Rotation == 'L') {
+            facingdirection--;
+            if (facingdirection == -1) {
+                facingdirection = 3;
+            }
+        }
+        else {
+            throw (new Error(`Unknown rotation current Move :${currentMove}:`));
+        }
+        switch (facingdirection) {
+            case 0: { //north
+                blockDirectionCount.north += parseInt(BlockCount);
+                break;
+            }
+            case 1: { //east
+                blockDirectionCount.east += parseInt(BlockCount);
+                break;
+            }
+            case 2: { //south
+                blockDirectionCount.north -= parseInt(BlockCount);
+                break;
+            }
+            case 3: { //west
+                blockDirectionCount.east -= parseInt(BlockCount);
+                break;
+            }
+        }
+        let postionID = `n${blockDirectionCount.north}e${blockDirectionCount.east}`;
+        if (!foundFlag) {
+            if (visitedPosition.includes(postionID)) {
+                blockDirectionCountFirstPosition = blockDirectionCount;
+                foundFlag = true;
+            }
+            visitedPosition.push(postionID);
+        }
+    });
+    //calculate total blocks away
+    let totalBlocksAway = Math.abs(blockDirectionCountFirstPosition.north) + Math.abs(blockDirectionCountFirstPosition.east);
+    console.log(`Star Two : ${totalBlocksAway}`);
+}
 starOneSolution();
+starTwoSolution();
--- 2016/day1.ts
+++ 2016/day1.ts
@@ -18,15 +18,16 @@ function starOneSolution() {
     };
     //count number of blocks away from starting position 
     data.forEach(currentMove => {
-        let splitMove = currentMove.slice(0, 1);
+        let Rotation = currentMove.slice(0, 1);
+        let BlockCount = currentMove.slice(1);
 
         //handle rotation
-        if (splitMove[0] == 'R') {
+        if (Rotation == 'R') {
             facingdirection++;
             if (facingdirection == 4) {
                 facingdirection = 0
             }
-        } else if (splitMove[0] == 'L') {
+        } else if (Rotation == 'L') {
             facingdirection--;
             if (facingdirection == -1) {
                 facingdirection = 3;
@@ -39,18 +40,18 @@ function starOneSolution() {
         switch (facingdirection) {
 
             case 0: {//north
-                blockDirectionCount.north += parseInt(splitMove[1]);
+                blockDirectionCount.north += parseInt(BlockCount);
                 break;
             }
             case 1: {//east
-                blockDirectionCount.east += parseInt(splitMove[1]);
+                blockDirectionCount.east += parseInt(BlockCount);
                 break;
             }
             case 2: {//south
-                blockDirectionCount.north -= parseInt(splitMove[1]);
+                blockDirectionCount.north -= parseInt(BlockCount);
                 break;
             } case 3: {//west
-                blockDirectionCount.east -= parseInt(splitMove[1]);
+                blockDirectionCount.east -= parseInt(BlockCount);
                 break;
             }
         }
@@ -67,7 +68,83 @@ function starOneSolution() {
 
 }
 
+function starTwoSolution() {
+    let testData3 = ['R5', 'L5', 'R5', 'R3'];
 
+    let facingdirection = 0; //0-N, 1-E, 2-S, 3-W
+    let blockDirectionCount = {
+        north: 0,
+        east: 0
+    };
+
+    let blockDirectionCountFirstPosition = {
+        north: 0,
+        east: 0
+    };
+
+    let foundFlag = false;
+    let visitedPosition: Array<string> = [];
+    //count number of blocks away from starting position 
+    data.forEach(currentMove => {
+        let Rotation = currentMove.slice(0, 1);
+        let BlockCount = currentMove.slice(1);
+
+        //handle rotation
+        if (Rotation == 'R') {
+            facingdirection++;
+            if (facingdirection == 4) {
+                facingdirection = 0
+            }
+        } else if (Rotation == 'L') {
+            facingdirection--;
+            if (facingdirection == -1) {
+                facingdirection = 3;
+            }
+        }
+        else {
+            throw (new Error(`Unknown rotation current Move :${currentMove}:`));
+        }
+
+        switch (facingdirection) {
+
+            case 0: {//north
+                blockDirectionCount.north += parseInt(BlockCount);
+                break;
+            }
+            case 1: {//east
+                blockDirectionCount.east += parseInt(BlockCount);
+                break;
+            }
+            case 2: {//south
+                blockDirectionCount.north -= parseInt(BlockCount);
+                break;
+            } case 3: {//west
+                blockDirectionCount.east -= parseInt(BlockCount);
+                break;
+            }
+        }
+
+        let postionID = `n${blockDirectionCount.north}e${blockDirectionCount.east}`;
+
+        if (!foundFlag) {
+            if (visitedPosition.includes(postionID)) {
+                blockDirectionCountFirstPosition = blockDirectionCount;
+                foundFlag = true;
+            }
+            visitedPosition.push(postionID);
+        }
+
+    })
+
+
+    //calculate total blocks away
+    let totalBlocksAway = Math.abs(blockDirectionCountFirstPosition.north) + Math.abs(blockDirectionCountFirstPosition.east);
+
+
+    console.log(`Star Two : ${totalBlocksAway}`);
+
+
+}
 
 
 
@@ -75,3 +152,4 @@ function starOneSolution() {
 
 
 starOneSolution();
+starTwoSolution();
