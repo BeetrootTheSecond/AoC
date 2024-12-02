7e137bc73afb867516275e0f7ddacce543cfdfd5
beetroot <aaronhorrell@hotmail.co.uk>
2024-12-02 17:59:08 +0000

    Day 2 Star 2 complete

--- source/2024/day2.ts
+++ source/2024/day2.ts
@@ -2,47 +2,68 @@ export const day2 = (data: string[], part: string )=> {
 
     let levels = data.map(level => level.split(' ').map(report => parseInt(report)));
     
-    const minDifference = 1;
-    const maxDifference = 3;
-
-    let safeLevels = levels.filter(reports => {
-
-        console.log(reports);
-
-            let isSafe = true;
-            let isDecreasing = (reports[1] - reports[0]) < 0;
-            for (let index = 1; index < reports.length; index++) {
-                let reportDiffence = reports[index-1] - reports[index];
-
-                if(reportDiffence < 0  == isDecreasing){
-                    isSafe = false;
-                    break;
-                }
-
-                let reportDiffenceAbs=  Math.abs(reportDiffence)
-                if(reportDiffenceAbs< minDifference || reportDiffenceAbs > maxDifference )                
-                {
-                    isSafe = false; 
-                    break;
-                }
-                
-            }
-            
-            console.log(isSafe);
-            return isSafe;
-
-        return false;
-        })
-
-
+   
 
     if(part == '1'){
-
+        let safeLevels = levels.filter(reports => isReportSafe(reports))
         console.log(`Star 1 Result : ${safeLevels.length} `);
         return;
     }
 
 
+    let safeLevels = levels.filter(reports => {
+        
+        let isSafe = isReportSafe(reports);
+        if(isSafe){
+            return isSafe;
+        }
+
+        for (let index = 0; index < reports.length; index++) {
+            let reportCopy = [...reports];
+            let newReport = reportCopy.splice(index,1);
+             let safeWithoutIndex = isReportSafe(reportCopy); 
+             if(safeWithoutIndex){
+                return safeWithoutIndex;
+            }      
+        }
+
+        return isSafe;
+        
+    
+    })
+
+
     console.log(`Star 2 Result : ${safeLevels.length} `);
     return;
+}
+
+function isReportSafe( reports: number[] ){
+
+    const minDifference = 1;
+    const maxDifference = 3;
+
+    console.log(reports);
+    
+    let isSafe = true;
+    let isDecreasing = (reports[1] - reports[0]) < 0;
+    for (let index = 1; index < reports.length; index++) {
+        let reportDiffence = reports[index-1] - reports[index];
+
+        if(reportDiffence < 0  == isDecreasing){
+            isSafe = false;
+            break;
+        }
+
+        let reportDiffenceAbs=  Math.abs(reportDiffence)
+        if(reportDiffenceAbs< minDifference || reportDiffenceAbs > maxDifference )                
+        {
+            isSafe = false; 
+            break;
+        }
+        
+    }
+    
+    console.log(isSafe);
+    return isSafe;
+
 }
