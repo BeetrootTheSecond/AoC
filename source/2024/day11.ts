8ab79387abf5137395ff680c5216a94dd876a6ae
beetroot <aaronhorrell@hotmail.co.uk>
2024-12-17 22:19:00 +0000

    Day 11 Star 2 complete

--- source/2024/day11.ts
+++ source/2024/day11.ts
@@ -1,39 +1,55 @@
 
-const modStone = (stone:number)=>{
-    let newStones = [];
-
-    if(stone == 0){
-        return [1];
-    }
-    let stoneString = stone.toString();
-    if(stoneString.length  %2 == 0){
-        //its Even;
-        let splitStoneLeft = parseInt(stoneString.split('').slice(0, (stoneString.length/2)).join(''));
-        let splitStoneRight = parseInt(stoneString.split('').slice((stoneString.length/2), stoneString.length).join(''));
-        return [splitStoneLeft,splitStoneRight];
-
-    }
-
-    return [stone*2024];
-}
 
 export const day11 = (data: string[], part: string) => {
+  let stones = data[0].split(" ").map((stone) => parseInt(stone));
+  let blinkCount = part == "1" ? 25 : 75;
 
-    let stones = data[0].split(' ').map(stone=>parseInt(stone));
-    let blinkCount= 25;
+  let OneAtATimeCache: Record<string, number> = {};
+const OneAtATimeMemor = (blinkNumber: number, stone: number): number => {
+  let key = `${blinkNumber}-${stone}`;
+  OneAtATimeCache[key] = OneAtATimeCache[key] || OneAtATime(blinkNumber, stone);
+  return OneAtATimeCache[key];
+};
 
-    for (let blinkNumber = 0; blinkNumber < blinkCount; blinkNumber++) {
-        stones = stones.map(stone=> modStone(stone)).flat();
-        
-    }
+const OneAtATime = (blinkNumber: number, stone: number): number => {
+  if (blinkNumber == blinkCount) {
+    return 1;
+  }
+  if (stone == 0) {
+    return OneAtATimeMemor(blinkNumber + 1, 1);
+  }
+  let stoneString = stone.toString();
+  if (stoneString.length % 2 == 0) {
+    //its Even;
+    let splitStoneLeft = parseInt(
+      stoneString
+        .split("")
+        .slice(0, stoneString.length / 2)
+        .join("")
+    );
+    let splitStoneRight = parseInt(
+      stoneString
+        .split("")
+        .slice(stoneString.length / 2, stoneString.length)
+        .join("")
+    );
+    return (
+      OneAtATimeMemor(blinkNumber + 1, splitStoneLeft) +
+      OneAtATimeMemor(blinkNumber + 1, splitStoneRight)
+    );
+  }
+  return OneAtATimeMemor(blinkNumber + 1, stone * 2024);
+};
 
+  let stonesResults = stones.map((stone: number) => {
+    return OneAtATimeMemor(0, stone);
+  });
 
+  let TotalStones = stonesResults.reduce(
+    (total, stoneCount) => (total += stoneCount),
+    0
+  );
 
-if(part== '1')
-{
-    let starOne = stones.length;
-    console.log(`Star One Results : ${starOne}`);
-    return starOne;
-}
-
-}
+  console.log(`Results : ${TotalStones}`);
+  return TotalStones;
+};
