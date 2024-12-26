dafce3cd0ea672a6a2ad422c9ba24e680c01bcbb
beetroot <aaronhorrell@hotmail.co.uk>
2024-12-26 22:02:17 +0000

    Reformated the output from the cli call to include a the result and a time

--- index.ts
+++ index.ts
@@ -10,29 +10,31 @@ program.command('adventofcode')
     .description('')
     .addArgument(new Argument('<year>', 'Year').choices(['2024']))
     .addArgument(new Argument('<day>', 'Day').choices(([...new Array(25)].map((_, i) => (i + 1).toString()))))
-    .addArgument(new Argument('<part>', 'Part').choices(['1','2','all']))
-    .option('-s, --sample', 'run using sample data',false)
-    .action((year,day:string,part,option)=>{
-        
-        console.log(`Year ${year} , Day ${day} , Part ${part} , Sample ${option.sample}`);
+    .addArgument(new Argument('<part>', 'Part').choices(['1', '2', 'all']))
+    .option('-s, --sample', 'run using sample data', false)
+    .action((year, day: string, part, option) => {
+
+        //console.log(`Year ${year} , Day ${day} , Part ${part} , Sample ${option.sample}`);
         // load data
 
         let data = fs.readFileSync(`.input/${year}/${day}.txt`, 'utf-8').split('\n');
-        if(option.sample){
+        if (option.sample) {
             data = fs.readFileSync(`.input/${year}/${day}.sample.txt`, 'utf-8').split('\n');
         }
-        
-        
 
-        for(const [key, fn] of Object.entries(y2024)){
-            if(key.replace("day","") == day){
+
+
+        for (const [key, fn] of Object.entries(y2024)) {
+            if (key.replace("day", "") == day) {
                 const startTime = performance.now();
-                fn(data, part);
+                let Result = fn(data, part);
                 const endTime = performance.now();
 
-                console.log(`Year 2024,  Day ${day}, part ${part}, sample ${option.sample}  ${endTime - startTime} milliseconds`)   
+                let ResultMeta = { Year: parseInt(year), Day: parseInt(day), Part: parseInt(part), Result, Time: `${endTime - startTime} milliseconds`, Sample: option.sample };
+
+                console.table([ResultMeta]);
                 break;
-             }
+            }
         }
     })
     .parse();
