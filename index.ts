e83be4d3982cd40e2253a5f6f69cba0d1fac4d39
beetroot <aaronhorrell@hotmail.co.uk>
2024-12-26 22:22:15 +0000

    Added file exist check for both the data file and sample file

--- index.ts
+++ index.ts
@@ -14,11 +14,26 @@ program.command('adventofcode')
     .option('-s, --sample', 'run using sample data', false)
     .action((year, day: string, part, option) => {
 
-        //console.log(`Year ${year} , Day ${day} , Part ${part} , Sample ${option.sample}`);
+        //check is data file exists if not create file and return error
+        let dataExists = fs.existsSync(`.input/${year}/${day}.txt`);
+        if (!dataExists) {
+
+            console.error(`Data file not found for day ${day} ${year}, an empty file has been created!`);
+            fs.writeFileSync(`.input/${year}/${day}.txt`, '');
+            return;
+        }
+
         // load data
 
         let data = fs.readFileSync(`.input/${year}/${day}.txt`, 'utf-8').split('\n');
         if (option.sample) {
+            let sampleExists = fs.existsSync(`.input/${year}/${day}.sample.txt`);
+            if (!sampleExists) {
+
+                console.error(`Sample file not found for day ${day} ${year}, an empty file has been created!`);
+                fs.writeFileSync(`.input/${year}/${day}.sample.txt`, '');
+                return;
+            }
             data = fs.readFileSync(`.input/${year}/${day}.sample.txt`, 'utf-8').split('\n');
         }
 
