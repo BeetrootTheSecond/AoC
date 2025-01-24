d9baae9927d8789aace17dd2bb75b49625ee20eb
beetroot <aaronhorrell@hotmail.co.uk>
2025-01-24 22:55:51 +0000

    2016 Day 3 Star 2 Complete

--- source/2016/day3.ts
+++ source/2016/day3.ts
@@ -6,14 +6,50 @@ export const day3 = (data: string[], part: string) => {
         .split("  ")
         .map((s) => parseInt(s.trim()))
         .filter((digit) => !isNaN(digit))
-        .sort((a,b)=>b-a)
+        //.sort((a,b)=>b-a)
     )
-    .filter(
+    if(part=='1'){
+    let starOne = triangle.filter(
       (possibleTrangle) =>
         possibleTrangle[0] < (possibleTrangle[1] + possibleTrangle[2])  &&
          possibleTrangle[1] < (possibleTrangle[0] + possibleTrangle[2]) &&
          possibleTrangle[2] < (possibleTrangle[1] + possibleTrangle[0])
     );
 
-  return triangle.length;
+  return starOne.length;
+    }
+
+let rotatedGrid :number[][]= [[],[],[]]
+
+for (let index = 0; index < triangle.length; index++) {
+  const element = triangle[index];
+  for (let j = 0; j < element.length; j++) {
+
+    rotatedGrid[j].push(element[j]);
+  }
+}
+
+//console.log(rotatedGrid);
+let triangleCount = rotatedGrid.map(column =>{
+  let count = 0;
+
+for (let index = 0; index < column.length; index+=3) {
+  const element = column[index];
+  const element1 = column[index+1];
+  const element2 = column[index+2];
+  if(element < (element1 + element2)  &&
+  element1 < (element + element2) &&
+  element2 < (element1 + element)){
+    count++;
+  }
+  
+}
+
+return count;
+
+}).reduce((total,current)=> total+=current,0)
+
+
+return triangleCount;
+    
 };
