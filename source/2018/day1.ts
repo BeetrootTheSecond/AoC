5b9021674f3b4580f5b6e731958a108b9d5af3ea
beetroot <aaronhorrell@hotmail.co.uk>
2025-01-31 17:45:42 +0000

    2018 Day 1 Star 2 Complete

--- source/2018/day1.ts
+++ source/2018/day1.ts
@@ -1,7 +1,29 @@
 export const day1 = (data: string[], part: string) => {
-  let part1 = data
+    let processedData = data
   .map(freq => { return {Symbol: freq.substring(0,1), number:parseInt(freq.substring(1))}})
-  .reduce((total,nextFreq) => nextFreq.Symbol == '+' ? total + nextFreq.number : total - nextFreq.number,0 );
+    if(part == '1'){
+  let part1 = processedData.reduce((total,nextFreq) => nextFreq.Symbol == '+' ? total + nextFreq.number : total - nextFreq.number,0 );
 
     return part1;
+}
+
+    let totalsReached : Record<number,number>=[];
+    let currentFreq = 0;
+
+    for (let index = 0; index < processedData.length; index++) {
+        const nextFreq = processedData[index];
+
+        currentFreq = nextFreq.Symbol == '+' ? currentFreq + nextFreq.number : currentFreq - nextFreq.number;
+        totalsReached[currentFreq] = totalsReached[currentFreq] + 1 || 1;
+        if(totalsReached[currentFreq] == 2 ){
+            break;
+        }
+
+        if(processedData.length-1 == index){
+            index = -1;
+        }
+        
+    }
+    return currentFreq;
+
 };
