25cb98c90d28ef8c3d383ba776c8d03c823516d8
beetroot <aaronhorrell@hotmail.co.uk>
2025-01-31 18:28:23 +0000

    2019 Day 1 Star 2 complete

--- source/2019/day1.ts
+++ source/2019/day1.ts
@@ -1,7 +1,24 @@
 export const day1 = (data: string[], part: string) => {
     let fuel = data.map(module => Math.floor(parseInt(module)/3)-2 );
 
+    if(part == '1'){
     let part1 = fuel.reduce((total, nextFuel)=> total+ nextFuel,0);
     return part1;
+    }
+
+    let part2 = fuel.reduce((total, nextFuel)=> total + (nextFuel + calulateFuel(nextFuel)),0);
+    return part2;
 
 };
+
+function calulateFuel(mass:number)
+{   
+    let fuel = Math.floor(mass/3)-2;
+    if(fuel >0){
+        fuel += calulateFuel(fuel);
+    }
+    else if ( fuel < 0 ){
+        fuel =0;
+    }
+    return fuel;
+}
