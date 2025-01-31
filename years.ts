ad7cb90c2591eab47be7ff09d668d71095202490
beetroot <aaronhorrell@hotmail.co.uk>
2025-01-31 17:05:23 +0000

    Adding the 2018 to CLI

--- years.ts
+++ years.ts
@@ -1,4 +1,5 @@
 export * as y2015 from "./source/2015";
 export * as y2016 from "./source/2016";
 export * as y2017 from "./source/2017";
+export * as y2018 from "./source/2018";
 export * as y2024 from "./source/2024";
